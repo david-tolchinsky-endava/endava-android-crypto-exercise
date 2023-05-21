10d9c306ae407472e2d33e7b9f9cf941f025483f
Luis <Mendoza>
2023-05-21 14:23:31 -0300

    Initial commit Styles and fonts loaded Home Fragment UI designed TODO: - Save selected theme - Market Fragment - Implement service API - Profile Fragment - Modify Splash screen - Possibly, create login screen

---
+++ app/src/main/java/com/mendoza/endavacryptoapp/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.mendoza.endavacryptoapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.mendoza.endavacryptoapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var menuItemSelected: BottomNavigationItemView
+    private lateinit var menuItemIndicator: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navHostFragment =  supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navGraph = navHostFragment.navController.navInflater.inflate(R.navigation.mobile_navigation)
+        val navController = navHostFragment.navController
+        navController.setGraph(graph = navGraph, startDestinationArgs = null)
+        navController.addOnDestinationChangedListener(this)
+
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        val view = binding.navView.findViewById<View>(destination.id)
+        view?.let {
+            addIndicatorAndIcon(it as BottomNavigationItemView)
+        }
+    }
+
+    private fun addIndicatorAndIcon(viewGroup: BottomNavigationItemView) {
+        if (::menuItemSelected.isInitialized && ::menuItemIndicator.isInitialized) {
+            menuItemSelected.removeView(menuItemIndicator)
+        }
+        menuItemSelected = viewGroup
+        menuItemIndicator = LayoutInflater.from(this)
+            .inflate(R.layout.bottom_navigation_indicator, viewGroup, false)
+        viewGroup.addView(menuItemIndicator)
+
+    }
+}
---
+++ app/src/main/java/com/mendoza/endavacryptoapp/ui/home/HomeFragment.kt
@@ -0,0 +1,51 @@
+package com.mendoza.endavacryptoapp.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.mendoza.endavacryptoapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.chipGroup.setOnCheckedStateChangeListener { _, checkedIds ->
+            if(checkedIds.contains(binding.chipLight.id)) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
